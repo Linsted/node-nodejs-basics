13d446746cbc5021d7499bb9fdc64fbee5a77d0b
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-02 21:46:55 +0200

    feat: add copy and create functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { access, cp } from "node:fs/promises";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+  const ERROR_MESSAGE = "FS operation failed";
+  const NOT_EXIST_ERROR_CODE = "ENOENT";
+  const ORIGINAL_FOLDER = "files";
+  const COPY_FOLDER = "files_copy";
+
+  const filesFolderPath = path.resolve(import.meta.dirname, ORIGINAL_FOLDER);
+  const filesCopyFolderPath = path.resolve(import.meta.dirname, COPY_FOLDER);
+
+  try {
+    await access(filesFolderPath);
+
+    try {
+      await access(filesCopyFolderPath);
+      throw new Error(ERROR_MESSAGE);
+    } catch (error) {
+      if (error?.code !== NOT_EXIST_ERROR_CODE) {
+        throw new Error(ERROR_MESSAGE);
+      }
+    }
+
+    await cp(filesFolderPath, filesCopyFolderPath, { recursive: true });
+  } catch (error) {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await copy();
