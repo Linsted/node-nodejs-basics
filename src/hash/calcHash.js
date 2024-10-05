6569a1669b22396acb1055e326f30e71ff7a37dc
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-05 13:53:24 +0200

    feat: finish hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import path from "path";
+import { createHash } from "node:crypto";
+import { open } from "node:fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const FILE_NAME = "fileToCalculateHashFor.txt";
+  const pathToFile = path.resolve(import.meta.dirname, "files", FILE_NAME);
+
+  try {
+    const fd = await open(pathToFile);
+    const hash = createHash("sha256");
+    const input = fd.createReadStream();
+
+    input.on("readable", () => {
+      const data = input.read();
+      if (data) {
+        hash.update(data);
+      } else {
+        console.log(`${hash.digest("hex")}`);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await calculateHash();
