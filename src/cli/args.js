0bbfbb149325b28af51610501de6a8a30266b329
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-05 09:55:23 +0200

    feat: finish cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argumentsArray = process.argv.splice(2, process.argv.length - 1);
+  const argumentsForLogging = [];
+
+  argumentsArray.forEach((element, index) => {
+    if (index % 2 === 0) {
+      argumentsForLogging.push(
+        `${element.slice(2)} is ${argumentsArray[index + 1]}${
+          index === argumentsArray.length - 2 ? "" : ","
+        }`
+      );
+    }
+  });
+
+  console.log(argumentsForLogging.join(" "));
 };
 
 parseArgs();
