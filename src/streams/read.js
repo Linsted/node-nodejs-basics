b5675f3fe022e54aaf246ad522fdb97c8c9cb3ae
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-06 09:22:21 +0200

    feat: finish Streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { open } from "node:fs/promises";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const FILE_NAME = "fileToRead.txt";
+
+  const pathToFile = path.resolve(import.meta.dirname, "files", FILE_NAME);
+
+  try {
+    const fd = await open(pathToFile);
+    const readable = fd.createReadStream();
+
+    let allData = "";
+
+    readable.on("data", (chunk) => {
+      allData += chunk;
+    });
+    readable.on("end", () => {
+      process.stdout.write(`${allData}\n`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { open } from "node:fs/promises";
+import path from "path";
+
+const getInput = () => {
+  return new Promise((resolve) => {
+    process.stdin.on("data", (data) => {
+      resolve(data.toString().trim());
+    });
+  });
+};
+
 const write = async () => {
-    // Write your code here 
+  const FILE_NAME = "fileToWrite.txt";
+
+  const pathToFile = path.resolve(import.meta.dirname, "files", FILE_NAME);
+
+  const inputValue = await getInput();
+
+  let filehandle = null;
+
+  try {
+    filehandle = await open(pathToFile, "w");
+    await filehandle.writeFile(inputValue);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (filehandle) {
+      await filehandle.close();
+    }
+    process.exit();
+  }
 };
 
 await write();
